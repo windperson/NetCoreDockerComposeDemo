580b0f98da172c0cbb493ba1ceae4731bce4d9df
Chen Yu Pao <windperson@gmail.com>
2019-07-09 10:44:51 +0800

    Add project files.

---
+++ WebFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["Message"] = "Hello from webfrontend";
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://mywebapi/api/values/1");
+                var response = await client.SendAsync(request);
+                ViewData["Message"] += " and " + await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
